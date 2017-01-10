72ddb87aa3a2408b86dcc86289541f871f2138b5
Adriaan <adriaan.bakker@hu.nl>
2017-01-10 10:51:23 +0100

    commit changes switches branch

--- HelloWorld/src/mainPackage/HelloWorld.java
+++ HelloWorld/src/mainPackage/HelloWorld.java
@@ -1,9 +1,14 @@
 package mainPackage;
 
 public class HelloWorld {
+	
+	// 
 
 	public static void main(String[] args) {
 		// here is a greeting
+		// with some extra comments
+		// that I made
+		// 
 		System.out.println("Hello, World");
 	}
 
